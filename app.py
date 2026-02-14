7fbbc57c42d3209df42ed57d44ffc547ed5e5ccb
Mhatre 141 <ayushmhatre141@gmail.com>
2026-02-14 16:22:37 +0530

    fix frontend display

--- app.py
+++ app.py
@@ -40,14 +40,16 @@ def predict_rain(data: PredictionRequest):
 
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "App is running 🚀"
+    return render_template("index.html")  # ✅ CORRECT
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # IMPORTANT
-    app.run(host="0.0.0.0", port=port)         # IMPORTANT
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
